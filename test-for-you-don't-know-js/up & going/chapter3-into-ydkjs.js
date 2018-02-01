80085f2e687ae2e4021647008a40e3e380ee40de
Jeff Chung <chungchi300@hotmail.com>
2018-02-01 09:59:41 +0800

    chapter 3

---
+++ test-for-you-don't-know-js/up & going/chapter3-into-ydkjs.js
@@ -0,0 +1,26 @@
+import _ from 'lodash';
+
+describe('YDKJS book,Read from top to bottom', () => {
+  it('javascript is not interpreted language,not compiled', () => {
+    //debunking the common misconception that JS is an "interpreted language" and therefore not compiled. Nope.
+    //JS compile language
+    //Var hositing is a prove for it
+    //Lexical Scope
+  });
+  it('this & object Prototypes', () => {
+    //the this is refering the instance depend of how it's called
+    //the this make you imagine the classes in java model but the object implementation in javascript is prototype(A very diffrent model to class Model)
+    //Object prototype system is more like "behaviour delegation"
+    //Delegation is an entirely different, and more powerful, design pattern, one that replaces the need to design with classes and inheritance
+    //Delegation over inheritance is more preferable in "Book Design Patterns Too"
+  });
+  it('types & grammar', () => {
+    //coercion
+  });
+  it('async & performance', () => {
+    //talk about promise
+    //talk about yield
+    //linear reason-ability
+    //ASM.js
+  });
+});
