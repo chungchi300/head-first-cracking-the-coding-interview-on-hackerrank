51f4bdc8cc3fce509afea997fd072f353a516f25
Jeff Chung <chungchi300@hotmail.com>
2018-02-08 17:12:01 +0800

    clear test case

--- __tests__/data-structures/stacks/equal-stacks.js
+++ __tests__/data-structures/stacks/equal-stacks.js
@@ -1,22 +1,4 @@
-process.stdin.resume();
-process.stdin.setEncoding('ascii');
-
-var input_stdin = '';
-var input_stdin_array = '';
-var input_currentline = 0;
 const _ = require('lodash');
-process.stdin.on('data', function(data) {
-  input_stdin += data;
-});
-
-process.stdin.on('end', function() {
-  input_stdin_array = input_stdin.split('\n');
-  main();
-});
-
-function readLine() {
-  return input_stdin_array[input_currentline++];
-}
 
 /////////////// ignore above this line ////////////////////
 function topestStack(indexedStacks) {
@@ -42,17 +24,18 @@ function stackHeight(stack) {
     return acc + num;
   }, 0);
 }
-function main() {
-  var n1_temp = readLine().split(' ');
-  var n1 = parseInt(n1_temp[0]);
-  var n2 = parseInt(n1_temp[1]);
-  var n3 = parseInt(n1_temp[2]);
-  h1 = readLine().split(' ');
-  h1 = h1.map(Number);
-  h2 = readLine().split(' ');
-  h2 = h2.map(Number);
-  h3 = readLine().split(' ');
-  h3 = h3.map(Number);
+function popToEven(indexedStacks) {
+  while (isAllStackSameHeight(indexedStacks) == false) {
+    const newTopestStack = topestStack(indexedStacks);
+    const popedElementHeight = newTopestStack.stack.shift();
+    newTopestStack.height -= popedElementHeight;
+  }
+  return indexedStacks;
+}
+describe('All', () => {
+  const h1 = [3, 2, 1, 1, 1];
+  const h2 = [4, 3, 2];
+  const h3 = [1, 1, 4, 1];
   const indexedStacks = {
     h1: {
       height: stackHeight(h1),
@@ -67,11 +50,11 @@ function main() {
       stack: h3,
     },
   };
-
-  while (isAllStackSameHeight(indexedStacks) == false) {
-    const newTopestStack = topestStack(indexedStacks);
-    const popedElementHeight = newTopestStack.stack.shift();
-    newTopestStack.height -= popedElementHeight;
-  }
-  console.log(topestStack(indexedStacks).height);
-}
+  it('topest', () => {
+    expect(topestStack(indexedStacks)).toBe(indexedStacks.h2);
+  });
+  it('All', () => {
+    popToEven(indexedStacks);
+    expect(topestStack(indexedStacks).height).toBe(5);
+  });
+});
