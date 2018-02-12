0529c723be84241c98685b5234b06567c47a42d4
Astri Jeff Chung <jeffchung@astri.org>
2018-02-13 06:29:23 +0800

    heap

---
+++ __tests__/data-structures/heap/heap.js
@@ -0,0 +1,17 @@
+describe('All', () => {
+  it('All', () => {
+    //heap is just a tree with 2 child node for each node
+    /*
+    class HeapNode{
+      HeapNode left;
+      HeapNode right;
+    }
+    */
+    //heap have 2 type,
+    //min ,the root always is the min,max,the root always is the max
+    /*
+      we will use array to implement the class
+https://www.youtube.com/watch?v=t0Cq6tVNRBA
+    */
+  });
+});
