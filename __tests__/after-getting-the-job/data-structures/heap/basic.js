0a8032cec51ff7728349a6787e69c9849436cd98
Jeff Chung <chungchi300@hotmail.com>
2018-02-22 12:09:26 +0800

    heap example

--- __tests__/after-getting-the-job/data-structures/heap/basic.js
+++ __tests__/after-getting-the-job/data-structures/heap/basic.js
@@ -1,3 +1,4 @@
+var buckets = require('buckets-js');
 describe('All', () => {
   it('All', () => {
     //heap is just a tree with 2 child node for each node
@@ -13,5 +14,22 @@ describe('All', () => {
       we will use array to implement the class
 https://www.youtube.com/watch?v=t0Cq6tVNRBA
     */
+    let minHeap = new buckets.Heap((a, b) => {
+      return a - b;
+    });
+    minHeap.add(4);
+    minHeap.add(1);
+    minHeap.add(2);
+    minHeap.add(3);
+    expect(minHeap.peek()).toBe(1);
+    let maxHeap = new buckets.Heap((a, b) => {
+      return -(a - b);
+    });
+    maxHeap.add(3);
+    expect(maxHeap.peek()).toBe(3);
+    maxHeap.add(4);
+    maxHeap.add(1);
+    maxHeap.add(2);
+    expect(maxHeap.peek()).toBe(4);
   });
 });
