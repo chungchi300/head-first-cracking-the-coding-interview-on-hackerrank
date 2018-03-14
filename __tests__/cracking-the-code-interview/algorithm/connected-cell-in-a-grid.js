c40b43ba7ce86faf2250f3f229a444fa62e08728
Jeff Chung <chungchi300@hotmail.com>
2018-03-14 13:54:46 +0800

    fix

--- __tests__/cracking-the-code-interview/algorithm/connected-cell-in-a-grid.js
+++ __tests__/cracking-the-code-interview/algorithm/connected-cell-in-a-grid.js
@@ -2,27 +2,42 @@ const _ = require('lodash');
 /*
 Learning Point -  Depth First Search algorithm
 1. Recursive function(Definition & searching point)
-2. When Searching,mutate the data to consume used data in order to let another search use proper data
+2. When Searching,mutate the data to consume used data in order to let another search use proper data.
+
+3. native function call stack size is 17964
+4. is over it->the solution are
+   * implement your stack ->trampolines
+
+   V8设置内存限制是由理由的，每个进程1.7G左右的内存足够作为服务进程来使用。如果超过这个限制首先考虑的是内存泄露，其次是超过单实例能力，最后实在没辙再从node启动项调整进程的可用最大内存。如果前两部没有做完直接修改只能是治标不治本。
+BTW： node app.js --max-stack-size 最大内存
 */
-function getMatrixConnectedSumAtSpecificPosition(matrix, row, col) {
+function getMatrixConnectedSumAtSpecificPosition(matrix, x, y) {
   //Stop point of recursive search - Use Boundary
   if (
-    row < 0 ||
-    row >= matrix.length ||
-    col < 0 ||
-    col > matrix[0].length ||
+    x < 0 ||
+    x >= matrix.length ||
+    y < 0 ||
+    y >= matrix[0].length ||
     matrix == null ||
-    matrix[row][col] == 0
+    matrix[x][y] == 0
   ) {
     return 0;
   }
-  matrix[row][col] = 0;
+  //switch to 0 so it would come back when travse
+  matrix[x][y] = 0;
   let size = 1;
   //searching
-  for (let r = row - 1; r <= row + 1; r++) {
-    for (let c = col - 1; c <= col + 1; c++) {
-      if (!(r == row && c == col)) {
-        size += getMatrixConnectedSumAtSpecificPosition(matrix, r, c);
+  for (let nextTraverseX = x - 1; nextTraverseX <= x + 1; nextTraverseX++) {
+    for (let nextTraverseY = y - 1; nextTraverseY <= y + 1; nextTraverseY++) {
+      //skip current,already counted
+      if (!(nextTraverseX == x && nextTraverseY == y)) {
+        // console.log(r, x, matrix.length, matrix.length, c, y);
+
+        size += getMatrixConnectedSumAtSpecificPosition(
+          matrix,
+          nextTraverseX,
+          nextTraverseY
+        );
       }
     }
   }
@@ -32,10 +47,11 @@ function getMatrixConnectedSumAtSpecificPosition(matrix, row, col) {
 function getLargestConnected(matrix) {
   let max = 0;
 
-  for (let row = 0; row < matrix.length; row++) {
-    for (let col = 0; col < matrix.length; col++) {
-      if (matrix[row][col] == 1) {
-        let size = getMatrixConnectedSumAtSpecificPosition(matrix, row, col);
+  for (let x = 0; x < matrix.length; x++) {
+    for (let y = 0; y < matrix.length; y++) {
+      if (matrix[x][y] == 1) {
+        let size = getMatrixConnectedSumAtSpecificPosition(matrix, x, y);
+
         max = Math.max(max, size);
       }
     }
@@ -45,7 +61,13 @@ function getLargestConnected(matrix) {
 describe('All - DFS Depth First Search', () => {
   it('get connectedMatrix sum', () => {
     expect(2 + 2).toBe(4);
-    let matrix = [[1, 1, 0, 0], [0, 1, 1, 0], [0, 0, 1, 0], [1, 0, 0, 0]];
-    expect(getLargestConnected(matrix)).toBe(5);
+    let matrix = [
+      [0, 0, 1, 1],
+      [0, 0, 1, 0],
+      [0, 1, 1, 0],
+      [0, 1, 0, 0],
+      [1, 1, 0, 0],
+    ];
+    expect(getLargestConnected(matrix)).toBe(8);
   });
 });
